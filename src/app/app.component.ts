d25735244a319663e336c49db7b7eb04e5fa223f
Kate <111576086+ShpakKate@users.noreply.github.com>
2023-04-09 14:23:04 +0300

    added unsubscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {UserInfoService} from "./shared/services/user-info.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   textOfUser: string | undefined;
   response: any;
   form!: FormGroup;
+  subscription?: Subscription | null;
   text = new FormControl('', [Validators.minLength(5)]);
 
   constructor(private userInfoService: UserInfoService) {}
@@ -23,7 +25,7 @@ export class AppComponent implements OnInit {
   }
 
   setData() {
-    this.userInfoService.postData({
+    this.subscription = this.userInfoService.postData({
       text: this.text.value as string,
       inventories: ['big5', 'neo']
     })
@@ -35,4 +37,10 @@ export class AppComponent implements OnInit {
     )
     this.form.reset();
   }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+    this.subscription = null;
+  }
+
 }
